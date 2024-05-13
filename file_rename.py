41cf0684796e58a0e2682e7afb5739063ddc25bd
iamrobik <robert.logovinsky@gmail.com>
2024-05-13 01:04:31 -0400

    First Commit

---
+++ file_rename.py
@@ -0,0 +1,14 @@
+import os
+from pathlib import Path
+
+# Input your personal directory path here
+directory_path = 'Enter Your Folder Path Here'
+
+for filename in os.listdir(directory_path):
+    old_file = Path(directory_path) / filename
+    if old_file.is_file():
+        new_file = old_file.with_suffix('.txt')
+        old_file.rename(new_file)
+
+print("All files have been renamed to .txt")
+print("All files have been renamed to .txt")
